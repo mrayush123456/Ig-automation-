8ac5154d3c182dd48bfd1d713bbd36b5e6f4f01d
mrayush123456 <yk7129388@gmail.com>
2024-12-05 06:36:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -182,4 +182,5 @@ def automate_instagram():
     return render_template_string(HTML_TEMPLATE)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
