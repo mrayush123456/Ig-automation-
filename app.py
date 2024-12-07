12810f3696f1eb11a844f07d3ff05221d18d01da
mrayush123456 <yk7129388@gmail.com>
2024-12-07 11:28:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -137,5 +137,5 @@ def instagram_messenger():
     return render_template_string(HTML_TEMPLATE)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-        
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
